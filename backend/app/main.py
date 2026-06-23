d284ad8fa4cbd699fc3db9a19d12d47d20a148c4
VishwaJeet-Nande <sachinnandekrd@gmail.com>
2026-06-23 14:01:57 +0530

    imports of database added

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,11 +7,20 @@ from app.api.risks import router as risk_router
 from app.api.decision import (router as decision_router)
 from app.api.digital_twin import ( router as digital_twin_router)
 from app.api.assistant import ( router as assistant_router)
+from app.database.base import Base
+from app.database.session import engine
+
+# import all models
+from app.models.user import User
 
 app = FastAPI(
     title="UrbanMindX API",
     version="1.0.0"
 )
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,7 +33,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(auth_router)
 app.include_router(complaint_router)
